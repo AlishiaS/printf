6317a628fd02048350a717976158276a5a44a5cc
AlishiaS <subramoneyalishia@gmail.com>
2022-07-12 18:30:53 +0200

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -7,15 +7,15 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list ap;
+	va_list arg;
 	int n;
 
 	if (!format)
 		return (-1);
 
-	va_start (ap, format);
-	n = _vprintf(format, ap);
-	va_end(ap);
+	va_start (arg, format);
+	n = _vprintf(stdout, format, arg);
+	va_end(arg);
 
 	return (n);
 }
