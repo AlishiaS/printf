2753e78c143c771144992c9c81aa7189680efd7b
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-07-23 19:14:20 +0200

    Revert "test files"
    This reverts commit 7e019c3703d51b20ad79377335d564ff606a2786.

--- test.c
+++ test.c
@@ -11,8 +11,13 @@ int main(void)
 {
 	int len;
    	int len2;
-	unsigned int ui;
-   	void *addr;
+	int alen;
+	int elen;
+	char c = 'z';
+	char *str = "sometimes    by";
+	/*char *str = NULL;*/
+	/*unsigned int ui;*/
+   	/*void *addr;*/
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
